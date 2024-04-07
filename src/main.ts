106780649696a6259465e4fddb876d1995daf610
kennizen <prachurjyagogoi123@gmail.com>
2024-04-07 20:45:19 +0530

    rtc connection successfull

--- src/main.ts
+++ src/main.ts
@@ -9,50 +9,61 @@ const servers = {
 };
 
 (() => {
+  // selector
   const selectNode = document.getElementById("peer-select");
-  const sender = document.getElementById('sender')
-  const receiver = document.getElementById("receiver")
+  const sender = document.getElementById("sender");
+  const receiver = document.getElementById("receiver");
 
+  selectNode?.addEventListener("change", handleSelectChange);
+
+  // sender
   const offerNode = document.getElementById("offer");
-  const createOfferBtn = document.getElementById("create-offer");
-  const copyOfferBtn = document.getElementById("copy-offer");
-  const iceCanNode = document.getElementById("ice-can");
-  const copyIceCanBtn = document.getElementById("copy-ice-can");
+  const createOfferBtn = document.getElementById("create-offer-btn");
+  const copyOfferBtn = document.getElementById("copy-offer-btn");
+  const acceptAnsNode = document.getElementById("accept-answer");
+  const acceptAnsBtn = document.getElementById("accept-answer-btn");
+
+  createOfferBtn?.addEventListener("click", handleCreateOffer);
+  copyOfferBtn?.addEventListener("click", handleOfferCopy);
+  acceptAnsBtn?.addEventListener("click", handleAcceptAnswer);
+
+  // receiver
+  const acceptOfferBtn = document.getElementById("accept-offer-btn");
+  const acceptOfferNode = document.getElementById("accept-offer");
   const ansNode = document.getElementById("answer");
-  const creatAnsBtn = document.getElementById("create-ans");
-  const copyAnsBtn = document.getElementById("copy-answer");
-  const addAnsNode = document.getElementById("add-answer");
-  const addAnsBtn = document.getElementById("add-answer-btn");
-  const addIceCanNode = document.getElementById("remote-ice-can");
-  const addIceCanBtn = document.getElementById("add-ice-can");
+  const copyAnsBtn = document.getElementById("copy-answer-btn");
+
+  acceptOfferBtn?.addEventListener("click", handleAcceptOffer);
+  copyAnsBtn?.addEventListener("click", handleAnswerCopy);
+
+  // ice candidates
+  const iceCanNode = document.getElementById("ice-can");
+  const copyIceCanBtn = document.getElementById("copy-ice-can-btn");
+  const remoteIceCanNode = document.getElementById("remote-ice-can");
+  const remodeIceCanBtn = document.getElementById("add-remote-ice-can-btn");
+
+  copyIceCanBtn?.addEventListener("click", handleIceCandidatesCopy);
+  remodeIceCanBtn?.addEventListener("click", handleAddIceCandidates);
 
   const peerConnection = new RTCPeerConnection(servers);
   const dataChannel = peerConnection.createDataChannel("myDataChannel");
 
-  selectNode?.addEventListener("change", handleSelectChange);
-
-  createOfferBtn?.addEventListener("click", handleCreateOffer);
-  copyOfferBtn?.addEventListener("click", handleOfferCopy);
-  copyIceCanBtn?.addEventListener("click", handleIceCandidatesCopy);
-  addAnsBtn?.addEventListener("click", handleAddAnswer);
-  creatAnsBtn?.addEventListener("click", handleCreateAnswer);
-  copyAnsBtn?.addEventListener("click", handleAnswerCopy);
+  dataChannel.addEventListener("open", () => console.log("data is transferrable"));
 
   peerConnection.onicecandidate = handleIceCandidate;
   peerConnection.oniceconnectionstatechange = handleIceConnectionStateChange;
 
   function handleSelectChange(e: Event) {
-    const val = (e.target as HTMLSelectElement).value
+    const val = (e.target as HTMLSelectElement).value;
 
-    if(!sender || !receiver) return;
+    if (!sender || !receiver) return;
 
-    if(val === "sender"){
+    if (val === "sender") {
       sender.style.display = "flex";
-      receiver.style.display = "none"
-    }
-    else if(val === "receiver"){
+      receiver.style.display = "none";
+    } else if (val === "receiver") {
       sender.style.display = "none";
-      receiver.style.display = "flex"
+      receiver.style.display = "flex";
     }
   }
 
@@ -63,10 +74,18 @@ const servers = {
     if (offerNode) offerNode.innerText = btoa(JSON.stringify(offer));
   }
 
-  async function handleCreateAnswer() {
+  async function handleAcceptOffer() {
+    if (!acceptOfferNode) return;
+
+    const offer = JSON.parse(atob(acceptOfferNode.textContent ?? "")) as RTCSessionDescriptionInit;
+    console.log("offer in receiver", offer);
+
+    await peerConnection.setRemoteDescription(offer);
+
     const answer = await peerConnection.createAnswer();
     await peerConnection.setLocalDescription(answer);
     console.log("answer", answer);
+
     if (ansNode) ansNode.innerText = btoa(JSON.stringify(answer));
   }
 
@@ -89,8 +108,11 @@ const servers = {
     }
   }
 
-  function handleIceConnectionStateChange() {
-    console.log("connection state", peerConnection.iceConnectionState);
+  async function handleAcceptAnswer() {
+    if (!acceptAnsNode) return;
+
+    const answer = JSON.parse(atob(acceptAnsNode.textContent ?? "")) as RTCSessionDescriptionInit;
+    await peerConnection.setRemoteDescription(answer);
   }
 
   async function handleIceCandidatesCopy() {
@@ -103,5 +125,17 @@ const servers = {
     await navigator.clipboard.writeText(JSON.stringify(candidates));
   }
 
-  async function handleAddAnswer() {}
+  async function handleAddIceCandidates() {
+    if (!remoteIceCanNode) return;
+
+    const candidates = JSON.parse(remoteIceCanNode.textContent ?? "") as string[];
+
+    candidates.map(async (can) => {
+      await peerConnection.addIceCandidate(JSON.parse(atob(can)));
+    });
+  }
+
+  function handleIceConnectionStateChange() {
+    console.log("connection state", peerConnection.iceConnectionState);
+  }
 })();
